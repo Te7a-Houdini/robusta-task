3dbab5576ca7ecbf7c1d2bcf48d6b93378e526c8
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-17 16:32:29 +0200

    Add method to return salary and bonuses for each employee

--- app/User.php
+++ app/User.php
@@ -32,4 +32,21 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function employeeSalariesAndBonuses()
+    {
+        return self::role('employee')->get()->map(function($employee){
+            return $employee->salaryAndBonus();
+        });
+    }
+
+    public function salaryAndBonus()
+    {
+        return [
+            'salary' => $this->salary,
+            'bonus' => $this->bonus_percentage ? 
+                        $this->salary * ($this->bonus_percentage/100) :
+                         $this->salary * (10 / 100)
+        ];
+    }
 }
