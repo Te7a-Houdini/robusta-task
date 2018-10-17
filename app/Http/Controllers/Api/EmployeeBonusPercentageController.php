ff05bcb7b16784cf5badfe523735cc52da22601d
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-17 17:20:45 +0200

    Add updating employee bonus percentage

--- app/Http/Controllers/Api/EmployeeBonusPercentageController.php
+++ app/Http/Controllers/Api/EmployeeBonusPercentageController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Http\Resources\UserResource;
 
 class EmployeeBonusPercentageController extends Controller
 {
-    public function update(Request $request)
+    public function update(Request $request,User $employee)
     {
-        
+        $request->validate([
+            'bonus_percentage' => 'required|numeric'
+        ]);
+
+        $employee->update(['bonus_percentage' => $request->bonus_percentage]);
+
+        return new UserResource($employee);
     }
 }
