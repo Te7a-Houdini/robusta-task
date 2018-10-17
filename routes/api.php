0f48e951b0b9382b4717791c71163154f7256a06
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-17 15:57:48 +0200

    Add salariesToBePaidController

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['namespace' => 'Api'],function(){
     Route::get('employees','EmployeesController@index');
+    Route::get('salaries-to-be-paid','SalariesToBePaidController@index');
 });
