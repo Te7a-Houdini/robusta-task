05754b5171aae651c44f8d5339eb75617781779d
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2018-10-19 02:47:05 +0200

    Add role:admin middleware on all routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace' => 'Api'],function(){
+Route::group(['namespace' => 'Api','middleware' => ['role:admin']],function(){
     Route::get('employees','EmployeesController@index');
     Route::post('employees','EmployeesController@store');
 
